0c14f580d2eb4458ff82e4ac3cb9c5b3bdf7172b
Leonardo Pang <leonardopang12@gmail.com>
2022-11-30 19:20:22 -0300

    userContext and Validate token

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,48 +3,30 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import UseForm from "../Hooks/UseForm";
-import { TOKEN_POST, USER_GET } from "../../api";
+import Error from "../Helper/Error";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
   const username = UseForm();
   const password = UseForm();
 
-  React.useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    if (token) {
-      getUser(token);
-    }
-  });
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
+  const { userLogin, error, loading } = React.useContext(UserContext)
 
   async function handleSubmit(event) {
     event.preventDefault();
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-      window.localStorage.setItem("token", json.token);
-      console.log(json);
+      userLogin(username.value, password.value)
     }
   }
 
   return (
-    <section>
-      <h1>Login</h1>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Entrar</Button>
+        {loading ? (<Button disabled>Carregando...</Button>) : (<Button>Entrar</Button>)}
+        <Error error={error} />
       </form>
       <Link to="/login/criar">Cadastrar</Link>
     </section>
