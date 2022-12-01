dd9611436bce02bb21ae50786f849e7b92a84280
Leonardo Pang <leonardopang12@gmail.com>
2022-12-01 14:38:27 -0300

    Login Background

--- src/UserContext.js
+++ src/UserContext.js
@@ -9,19 +9,19 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const userLogout = React.useCallback(
     async function () {
-      setData(null)
-      setError(null)
-      setLoading(false)
-      setLogin(false)
-      window.localStorage.removeItem('token')
-      navigate('/login')
-    }, [navigate],)
-
-
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+      window.localStorage.removeItem('token');
+      navigate('/login');
+    },
+    [navigate],
+  );
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
@@ -33,46 +33,48 @@ export const UserStorage = ({ children }) => {
 
   async function userLogin(username, password) {
     try {
-      setError(null)
-      setLoading(true)
+      setError(null);
+      setLoading(true);
       const { url, options } = TOKEN_POST({ username, password });
       const tokenRes = await fetch(url, options);
-      if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`)
+      if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
       const { token } = await tokenRes.json();
       window.localStorage.setItem('token', token);
       await getUser(token);
-      navigate('/conta')
+      navigate('/conta');
     } catch (err) {
-      setError(err.message)
-      setLogin(false)
+      setError(err.message);
+      setLogin(false);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
   React.useEffect(() => {
     async function autoLogin() {
-      const token = window.localStorage.getItem('token')
+      const token = window.localStorage.getItem('token');
       if (token) {
         try {
-          setError(null)
-          setLoading(true)
-          const { url, options } = TOKEN_VALIDATE_POST(token)
-          const response = await fetch(url, options)
-          if (!response.ok) throw new Error('Token inválidao')
-          await getUser(token)
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error('Token inválido');
+          await getUser(token);
         } catch (err) {
-          userLogout()
+          userLogout();
         } finally {
-          setLoading(false)
+          setLoading(false);
         }
       }
     }
-    autoLogin()
-  }, [userLogout])
+    autoLogin();
+  }, [userLogout]);
 
   return (
-    <UserContext.Provider value={{ userLogin, data, error, loading, login }}>
+    <UserContext.Provider
+      value={{ userLogin, userLogout, data, error, loading, login }}
+    >
       {children}
     </UserContext.Provider>
   );
